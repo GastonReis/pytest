8e4634ea81533d10f914d3ae7fa7f35d224576f5
mauroreyes21 <39366918+mauroreyes21@users.noreply.github.com>
2022-04-08 13:48:19 -0300

    added assertion msg

--- e2e/tests/test_login.py
+++ e2e/tests/test_login.py
@@ -4,5 +4,5 @@ import pytest
 @pytest.mark.parametrize("user,password", [("standard_user", "secret_sauce"), ("locked_out_user", "secret_sauce")])
 def test_login_sauce(loginPage, user, password):
     loginPage.do_login(user, password)
-    assert loginPage.was_login_successful()
+    assert loginPage.was_login_successful(), "login failed"
 
